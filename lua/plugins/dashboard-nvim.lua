1f65167d36cba157972ac468c80cda868d3d590b
jay-zsh <p204654297@outlook.com>
2026-01-26 21:11:57 +0800

    feat(dashboard): Add header to Most Recently Used (MRU) section
    Addresses user request to 'beautify below most recent files' by providing a clear title for the MRU section in dashboard-nvim.

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,52 @@
+return {
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-lualine/lualine.nvim",
+		"akinsho/bufferline.nvim",
+	},
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				-- [关键修改] 控制 UI 元素的隐藏行为
+				hide = {
+					statusline = false, -- 如果设为 true，则在 dashboard 隐藏底部状态栏
+					tabline = false, -- [重要] 设为 false，就不会隐藏顶部的 Buffer 栏了
+					winbar = false, -- 是否隐藏 winbar
+				},
+
+				header = {
+					"███████╗ █████╗ ███████╗██╗   ██╗",
+					"██╔════╝██╔══██╗██╔════╝╚██╗ ██╔╝",
+					"█████╗  ███████║███████╗ ╚████╔╝ ",
+					"██╔══╝  ██╔══██║╚════██║  ╚██╔╝  ",
+					"███████╗██║  ██║███████║   ██║   ",
+					"╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ",
+				},
+				shortcut = {
+					{ desc = "󰈙 New File", group = "@property", action = "enew", key = "n" },
+					{ desc = "󰄕 Recent Files", group = "@property", action = "Telescope oldfiles", key = "r" },
+					{ desc = "󰐕 Find File", group = "@property", action = "Telescope find_files", key = "f" },
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy sync", key = "u" },
+					{ desc = "󰉓 Settings", group = "@property", action = "e ~/.config/nvim/init.lua", key = "s" },
+					{ desc = "󰐒 Quit", group = "@property", action = "qa", key = "q" },
+				},
+				project = {
+					enable = true,
+					limit = 8,
+					action = "Telescope find_files cwd=",
+				},
+				mru = {
+					enable = true,
+					limit = 10,
+					header = "󰄕 Most Recent Files",
+				},
+				footer = {},
+			},
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>d", ":Dashboard<CR>", { noremap = true, silent = true })
+	end,
+}
